71bf218fb776b96801c3b8b406c8a3f5445a2bc7
Ciro <174917541+cirosardinha@users.noreply.github.com>
2024-11-22 11:49:34 -0300

    feat: config routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { AddBookComponent } from './components/add-book/add-book.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'books',
+    component: BookListComponent,
+  },
+  {
+    path: 'add-book',
+    component: AddBookComponent,
+  },
+];
