12c9e9e351a2eb7304a3cfb0a296a41c3169db19
Ciro <174917541+cirosardinha@users.noreply.github.com>
2024-11-22 12:12:17 -0300

    feat: create book service

---
+++ src/app/services/book.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Book } from '../../types/book.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BookService {
+  private books: Book[] = [];
+
+  addBook(book: Book) {
+    this.books.push(book);
+  }
+
+  getBooks() {
+    return this.books;
+  }
+
+  deleteBook(id: number) {
+    this.books = this.books.filter((book) => book.id !== id);
+  }
+}
